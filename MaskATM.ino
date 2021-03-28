db3ea8793a8b011aecf399d8a0529d00d2781336
r3wnad <73693763+r3wnad@users.noreply.github.com>
2021-03-28 12:11:35 +0300

    Refining code Should be better

--- MaskATM.ino
+++ MaskATM.ino
@@ -1,21 +1,15 @@
 
 #include <Keypad.h>
-
 #include <LiquidCrystal.h>
 #include <SR04.h>
 
 #include "SR04.h"
 #define TRIG_PIN 24
 #define ECHO_PIN 22
-//#define TRIG_PIN 3
-//#define ECHO_PIN 2
-
 
 // http://www.arduino.cc/en/Tutorial/LiquidCrystal
 // */
 
-// include the library code:
-#include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
@@ -41,14 +35,14 @@ SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 
 
 void setup() {
-   Serial.begin(9600);
+  Serial.begin(9600);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   
   pinMode(buzzer, OUTPUT); 
   pinMode(ledPin, OUTPUT);
   
- digitalWrite(ledPin, LOW);
+  digitalWrite(ledPin, LOW);
   
   
 }
@@ -68,45 +62,46 @@ void loop() {
     digitalWrite(buzzer,HIGH);
     while (entry==0){
       
-     char customKey = customKeypad.getKey();
-     if (customKey=='0')
-      takenMasks = takenMasks*10+0;
-     if (customKey=='1')
-      takenMasks = takenMasks*10+1;
-     if (customKey=='2')
-      takenMasks = takenMasks*10+2;
-     if (customKey=='3')
-      takenMasks = takenMasks*10+3;
-     if (customKey=='4')
-      takenMasks = takenMasks*10+4;
-     if (customKey=='5')
-      takenMasks = takenMasks*10+5;
-     if (customKey=='6')
-      takenMasks = takenMasks*10+6;
-     if (customKey=='7')
-      takenMasks = takenMasks*10+7;
-     if (customKey=='8')
-      takenMasks = takenMasks*10+8;
-     if (customKey=='9')
-      takenMasks = takenMasks*10+9;
-     if (customKey=='C')
-      takenMasks = takenMasks/10;
-     if (customKey=='A'){
-      if (takenMasks>remMasks){
-      lcd.setCursor(0,0);
-      lcd.print("Excess amount");
-      delay(2000);
-      takenMasks = 0;
+      char customKey = customKeypad.getKey();
+      if (customKey=='0')
+        takenMasks = takenMasks*10+0;
+      if (customKey=='1')
+        takenMasks = takenMasks*10+1;
+      if (customKey=='2')
+        takenMasks = takenMasks*10+2;
+      if (customKey=='3')
+        takenMasks = takenMasks*10+3;
+      if (customKey=='4')
+        takenMasks = takenMasks*10+4;
+      if (customKey=='5')
+        takenMasks = takenMasks*10+5;
+      if (customKey=='6')
+        takenMasks = takenMasks*10+6;
+      if (customKey=='7')
+        takenMasks = takenMasks*10+7;
+      if (customKey=='8')
+        takenMasks = takenMasks*10+8;
+      if (customKey=='9')
+        takenMasks = takenMasks*10+9;
+      if (customKey=='C')
+        takenMasks = takenMasks/10;
+      if (customKey=='A'){
+        if (takenMasks>remMasks){
+        lcd.setCursor(0,0);
+        lcd.print("Excess amount");
+        delay(2000);
+        takenMasks = 0;
+        }
+        entry = 1;
+        break;
       }
-      entry = 1;
-      break;}
-     else{
-      lcd.setCursor(0,0);
-      lcd.print("Enter 0-9");
-      lcd.setCursor(0,1);
-      lcd.print("               ");
-      //break;
-     }
+      else{
+        lcd.setCursor(0,0);
+        lcd.print("Enter 0-9");
+        lcd.setCursor(0,1);
+        lcd.print("               ");
+        //break;
+      }
     String TakenMasks = String(takenMasks);
     lcd.setCursor(0, 1);
     lcd.print(TakenMasks);
@@ -120,7 +115,7 @@ void loop() {
     String TakenMasks = String(takenMasks);
     lcd.print(TakenMasks);
     delay(3000);
-    }
+  }
   remMasks = remMasks-takenMasks;
   Remmasks = String(remMasks);
 }
